b035e333fc2c1c9614654f7127136b4113baae04
Kushal SM <146661162+mrkushalsm@users.noreply.github.com>
2025-09-17 23:52:56 +0530

    fix: fix dashboard page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import AdminPage from './pages/AdminPage';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="min-h-screen bg-base-200 flex justify-center items-center p-4">
-      <AdminPage />
+      {isLoggedIn ? <Dashboard setIsLoggedIn={setIsLoggedIn} /> : <Login setIsLoggedIn={setIsLoggedIn} />}
     </div>
   );
 }
